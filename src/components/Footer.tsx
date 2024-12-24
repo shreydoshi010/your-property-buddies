61b1533509110693629cabaab0fb85183eeefd8c
Tej parekh <tej.wiztech@gmail.com>
2024-12-24 19:21:20 +0530

    small footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,12 +43,12 @@ const Footer = () => {
                     alt="Footer Wave"
                     className="-mt-1 w-full"
                 />
-                <div className='mx-14 sm:mx-7 pt-20 pb-10'>
-                    <div className='pb-10 flex justify-between sm:flex-col sm:gap-8'>
+                <div className='mx-14 sm:mx-7 md:mx-7 pt-20 pb-10'>
+                    <div className='pb-10 flex justify-between sm:flex-col sm:gap-8 md:flex-col md:gap-8'>
                         <div>
                             <Image src={logo} alt='logo' className='h-auto w-[172px]' width={100} height={100} />
                             <Typography variant='h4' className='mt-6'>From buying to selling real estate, we’ve got you covered!</Typography>
-                            <div className='mt-10 sm:hidden'>
+                            <div className='mt-10 sm:hidden md:hidden'>
                                 <ul className='flex gap-4'>
                                     <li>
                                         <Link href='/'><Image src={Instagram} alt="instagram" /></Link>
@@ -59,7 +59,7 @@ const Footer = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div className='flex gap-20 sm:gap-0 sm:justify-between mr-7 sm:mr-0'>
+                        <div className='flex gap-20 sm:gap-0 sm:justify-between mr-7 sm:mr-0 md:gap-0 md:justify-between md:mr-0'>
                             {footerTabs.map((tab, index) => (
                                 <div key={index}>
                                     <Typography variant='h4'>{tab.title}</Typography>
@@ -76,7 +76,7 @@ const Footer = () => {
                             ))}
                         </div>
                     </div>
-                    <div className='sm:pb-7 xl:hidden md:hidden xxl:hidden'>
+                    <div className='sm:pb-7 hidden sm:block md:block md:pb-7'>
                         <ul className='flex gap-4'>
                             <li>
                                 <Link href='/'><Image src={Instagram} alt="instagram" /></Link>
@@ -86,10 +86,10 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
-                    <div className='py-4 border-t border-b border-white flex justify-between sm:flex-col sm:gap-3'>
+                    <div className='py-4 border-t border-b border-white flex justify-between sm:flex-col sm:gap-3 md:flex-col md:gap-3'>
                         <Typography variant='body2'>© 2024 Your Property Buddies. All rights reserved.</Typography>
                         <div>
-                            <ul className='flex gap-20 sm:gap-1 sm:justify-between sm:flex-col '>
+                            <ul className='flex gap-20 sm:gap-1 sm:justify-between sm:flex-col md:gap-1 md:justify-between md:flex-col'>
                                 <li>
                                     <Link href='/' className='flex gap-4'><Image src={ExternalLink} alt="link" /><Typography variant='body2'>Privacy policy</Typography></Link>
                                 </li>
@@ -100,7 +100,7 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className='mt-6'>
-                        <p className='text-base sm:text-[14px] font-normal font-lato w-[750px] sm:w-auto'>Nothing on this page constitutes legal advice. For specific legal advice related to real estate matters, please consult with a Real Estate Legal Expert.</p>
+                        <p className='md:w-auto text-base sm:text-[14px] font-normal font-lato w-[750px] sm:w-auto md:text-[14px]'>Nothing on this page constitutes legal advice. For specific legal advice related to real estate matters, please consult with a Real Estate Legal Expert.</p>
                     </div>
                 </div>
             </div>
